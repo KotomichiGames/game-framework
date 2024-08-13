c68755f4c258be0a22085050aa2343f7674408e9
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-13 22:27:28 +0300

    Add window example project
    Included new CMakeLists.txt in 'code' directory to define the window_example project. Added 'main.cpp' with a simple window creation, update loop, and cleanup sequence.

---
+++ code/main.cpp
@@ -0,0 +1,22 @@
+#include <window.hpp>
+
+int main()
+{
+    engine::Window window;
+    window.create("Window", 800, 600);
+
+    // drawing initialization
+
+    while (!window.is_closed())
+    {
+        // drawing
+
+        window.update();
+    }
+
+    // drawing cleanup
+
+    window.destroy();
+
+    return 0;
+}
