a0f3ea7a1551bdf71b47daddd8f28254e9363625
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-15 16:59:39 +0300

    Refactor window creation to use a factory object
    Replaced direct window creation with a factory pattern to improve modularity and preparation for future extensions. Updated main loop to reflect new object handling and constants for window dimensions.

--- code/platform/window/main.cpp
+++ code/platform/window/main.cpp
@@ -1,22 +1,27 @@
-#include <glfw/window.hpp>
+#include <glfw/factory.hpp>
 
 int main()
 {
-    engine::Window window;
-    window.create("Window", 800, 600);
+    constexpr int width  = 800;
+    constexpr int height = 600;
+
+    engine::glfw::Factory platform_factory;
+
+    const auto window = platform_factory.create_window();
+    window->create("Window", width, height);
 
     // drawing initialization
 
-    while (!window.is_closed())
+    while (!window->is_closed())
     {
         // drawing
 
-        window.update();
+        window->update();
     }
 
     // drawing cleanup
 
-    window.destroy();
+    window->destroy();
 
     return 0;
 }
