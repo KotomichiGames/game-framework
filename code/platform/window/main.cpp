ff5c1ac27a963d7e14b12198228a734b0b9d2eff
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-19 20:52:50 +0300

    Separate GL and Win32 factory creation
    Previously, the code used only Win32 factory for window creation and commented out OpenGL-related code. This update creates both GL and Win32 factories and fully integrates OpenGL context management, including initializing and clearing the context.

--- code/platform/window/main.cpp
+++ code/platform/window/main.cpp
@@ -6,31 +6,31 @@
 
 int main()
 {
-    //const auto factory = std::make_shared<engine::glfw::Factory>();
-    const auto factory = std::make_shared<engine::win32::Factory>();
+    const auto glfw_factory  = std::make_shared<engine::glfw::Factory>();
+    const auto win32_factory = std::make_shared<engine::win32::Factory>();
 
     auto& window = engine::WindowInstance::instance();
-    window.create(factory, { "Window" });
+    window.create(glfw_factory, { "Window" });
 
-    const auto platform = factory->create_platform();
-    const auto context  = factory->create_context();
+    const auto platform = glfw_factory->create_platform();
+    const auto context  = win32_factory->create_context();
     context->create(window.handle());
 
-    //gladLoadGL();
+    gladLoadGL();
 
-    //glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 
     while (window.is_active())
     {
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         window.update();
 
-        //context->update();
+        context->update();
         platform->update();
     }
 
-    //context->destroy();
+    context->destroy();
 
     window.destroy();
     return 0;
