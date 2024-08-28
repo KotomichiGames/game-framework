6d55fb43be9e0da74ff280efd57dadf8fc58a5fe
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-28 21:53:33 +0300

    Refactor shader names and remove WindowFactory
    Updated shader stage file names for better clarity. Removed WindowFactory usage and made necessary adjustments to the WindowManager setup. Simplified color clearing by using a predefined gray color constant.

--- code/graphics/shaders/main.cpp
+++ code/graphics/shaders/main.cpp
@@ -1,6 +1,6 @@
-#include <core/window_factory.hpp>
 #include <core/window_manager.hpp>
 #include <core/file.hpp>
+#include <core/rgb.hpp>
 
 #include <opengl/functions.hpp>
 #include <opengl/commands.hpp>
@@ -18,9 +18,7 @@ using namespace engine;
 
 int32_t main()
 {
-    const auto factory = core::WindowFactory::create_factory();
-
-    core::WindowManager::instance().create(factory, { .title = "Shaders" });
+    core::WindowManager::instance().create({ .title = "Shaders" });
     core::WindowManager::instance().open();
 
     gl::Functions::load_core();
@@ -28,38 +26,38 @@ int32_t main()
 
     gladLoadGL();
 
-    const auto vertex_stage_data   = core::File::read("diffuse_vert.glsl");
-    const auto fragment_stage_data = core::File::read("diffuse_frag.glsl");
+    const auto vertex_stage_data   = core::File::read("default_shader.vert");
+    const auto fragment_stage_data = core::File::read("default_shader.frag");
 
-    const char* vertex_stage_temp = vertex_stage_data.data();
+    const auto vertex_stage_temp   = vertex_stage_data.data();
     const auto fragment_stage_temp = fragment_stage_data.data();
 
-    gl::ShaderStage vertex_shader { gl::vertex_shader };
-    vertex_shader.create();
-    glShaderSource(vertex_shader.handle(), 1, &vertex_stage_temp, nullptr);
-    glCompileShader(vertex_shader.handle());
+    gl::ShaderStage vertex_stage { gl::vertex_stage };
+    vertex_stage.create();
+    glShaderSource(vertex_stage.handle(), 1, &vertex_stage_temp, nullptr);
+    glCompileShader(vertex_stage.handle());
 
-    gl::ShaderStage fragment_shader { gl::fragment_shader };
-    fragment_shader.create();
-    glShaderSource(fragment_shader.handle(), 1, &fragment_stage_temp, nullptr);
-    glCompileShader(fragment_shader.handle());
+    gl::ShaderStage fragment_stage { gl::fragment_stage };
+    fragment_stage.create();
+    glShaderSource(fragment_stage.handle(), 1, &fragment_stage_temp, nullptr);
+    glCompileShader(fragment_stage.handle());
 
-    const uint32_t diffuse_shader = glCreateProgram();
-    glAttachShader(diffuse_shader, vertex_shader.handle());
-    glAttachShader(diffuse_shader, fragment_shader.handle());
-    glLinkProgram(diffuse_shader);
+    const uint32_t default_shader = glCreateProgram();
+    glAttachShader(default_shader, vertex_stage.handle());
+    glAttachShader(default_shader, fragment_stage.handle());
+    glLinkProgram(default_shader);
 
     GLint success;
-    glGetProgramiv(diffuse_shader, GL_LINK_STATUS, &success);
+    glGetProgramiv(default_shader, GL_LINK_STATUS, &success);
     if (!success)
     {
         GLchar info_log[512];
-        glGetProgramInfoLog(diffuse_shader, 512, nullptr, info_log);
+        glGetProgramInfoLog(default_shader, 512, nullptr, info_log);
         std::cerr << "Shader program linking failed:\n" << info_log << std::endl;
     }
 
-    vertex_shader.destroy();
-    fragment_shader.destroy();
+    vertex_stage.destroy();
+    fragment_stage.destroy();
 
     const std::vector<math::vec3> vertices
     {
@@ -77,25 +75,25 @@ int32_t main()
 
     gl::Buffer vertex_buffer;
     vertex_buffer.create();
-    vertex_buffer.data(base::buffer_data::create_from(vertices), gl::static_draw);
+    vertex_buffer.data(core::buffer_data::create_from(vertices), gl::static_draw);
 
     gl::Buffer indices_buffer;
     indices_buffer.create();
-    indices_buffer.data(base::buffer_data::create_from(indices), gl::static_draw);
+    indices_buffer.data(core::buffer_data::create_from(indices), gl::static_draw);
 
     gl::VertexArray vertex_array;
     vertex_array.create();
-    vertex_array.attach_vertex_buffer(vertex_buffer, sizeof(math::vec3 ));
+    vertex_array.attach_vertex_buffer(vertex_buffer, sizeof(math::vec3));
     vertex_array.attach_indices_buffer(indices_buffer);
-    vertex_array.attribute({ 0, 3, gl::type_float, 0 });
+    vertex_array.attribute({ 0, 3, gl::type_float });
 
-    gl::Commands::clear(0.5f, 0.5f, 0.5f);
+    gl::Commands::clear(core::color::gray);
 
     while (core::WindowManager::instance().is_active())
     {
         gl::Commands::clear(gl::color_buffer_bit);
 
-        glUseProgram(diffuse_shader);
+        glUseProgram(default_shader);
 
         vertex_array.bind();
         gl::Commands::draw_elements(gl::triangles, indices.size());
@@ -107,7 +105,7 @@ int32_t main()
     indices_buffer.destroy();
     vertex_array.destroy();
 
-    glDeleteProgram(diffuse_shader);
+    glDeleteProgram(default_shader);
 
     core::WindowManager::instance().destroy();
     return 0;
